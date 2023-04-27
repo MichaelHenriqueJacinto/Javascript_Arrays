bb6e17cedb01f404f3a68143e7a03ad901bdf91b
Michael Henrique <maicon_mhl@hotmail.com>
2023-04-27 02:45:43 -0300

    iniciando aula1

---
+++ aula1/media.js
@@ -0,0 +1,15 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+const notas = [
+            // posições
+    10,     // 0
+    6.5,    // 1
+    8,      // 2
+    7.5,    // 3
+]
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media)
